3da85b2531c86d5491e59678b95ff374e79b2be0
Yormungand <evdevosh@gmail.com>
2026-05-28 12:21:08 +0200

    refactor: remove unused project pages and associated composables to clean up source directory

--- src/pages/index.vue
+++ src/pages/index.vue
@@ -1,38 +1,9 @@
 <template>
-  <div class="flex flex-col min-h-screen">
-    <Header 
-      class="sticky top-0 z-50 transition-all duration-500 rounded-b-3xl" 
-      :class="{ 'bg-white/80 dark:bg-[#050505]/80 shadow-sm': themeStore.settings.themeName === 'Default' }"
-    />
-    <DashboardMain v-if="auth.isAuthenticated" />
-    <Main v-else class="grow" />
-    <Footer />
-  </div>
+  <ExTestCleanWorkspace />
 </template>
 
-
 <script setup>
-import Header from "@/widgets/header/ui/Header.vue";
-import Main from "@/widgets/main/Main.vue";
-import DashboardMain from "@/widgets/dashboard/ui/DashboardMain.vue";
-import Footer from "@/widgets/footer/Footer.vue";
+import ExTestCleanWorkspace from '~/widgets/test-clean/ui/ExTestCleanWorkspace.vue'
 
-import { onMounted } from 'vue'
-import { useForumStore } from "~/features/store/useForum";
-import { useAuthStore } from "~/entities/user/auth.store";
-import { useThemeStore } from "~/features/store/useTheme";
-
-const forum = useForumStore()
-const auth = useAuthStore()
-const themeStore = useThemeStore()
-
-onMounted(async () => {
-  await forum.fetchThreadList()
-})
-
-definePageMeta({
-  public: true
-})
-
+definePageMeta({ layout: false })
 </script>
-
