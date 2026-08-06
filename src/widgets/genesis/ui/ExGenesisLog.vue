16a13f3e1489b6eef23db798dc29865d6b8a823a
Yormungand <evdevosh@gmail.com>
2026-08-06 07:00:53 +0200

    style: reduce width of ExGenesisLog filters panel to 562px

--- src/widgets/genesis/ui/ExGenesisLog.vue
+++ src/widgets/genesis/ui/ExGenesisLog.vue
@@ -883,7 +883,7 @@
 
     <div
       v-if="showFiltersPanel && !showNodeMap && viewType === 'cube' && isHudVisible && !isTradeEntryOpen && !isTimeTreeFullscreen"
-      class="pointer-events-auto absolute left-1/2 top-8 z-[10010] w-[674px] -translate-x-1/2"
+      class="pointer-events-auto absolute left-1/2 top-8 z-[10010] w-[562px] -translate-x-1/2"
     >
       <ExPanel variant="light" :no-padding="true" :show-corners="true" class="!w-full overflow-visible">
         <ExVerticalTradeList
